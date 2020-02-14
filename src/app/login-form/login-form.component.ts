019bf8397dedf611b0a3ecb5af710d687aa12a2c
Sean Swanson <sean-swan@hotmail.com>
2020-02-13 21:12:03 -0800

    Got landing page to a point where real login will be easier to hook up.

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -9,4 +9,12 @@ export class LoginFormComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  logIn(e: Event) {
+    // TODO: Make work.
+
+    console.log(e);
+    document.querySelector(".landing-container").classList.add("hidden");
+    document.querySelector(".dashboard-container").classList.remove("hidden");
+  }
 }
